f4443a67264d82432b47eba1bfb1bd292c61dade
call203 <call203@daum.net>
2024-04-09 20:57:46 +0100

    fix build file

--- ctypes_test.py
+++ ctypes_test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('./aes')
-from aes import sub_bytes
+from aes import bytes2matrix,sub_bytes
 import unittest
 import random
 import ctypes
@@ -11,15 +11,15 @@ rijndael = ctypes.CDLL('./rijndael.so')
 class TestEncryption(unittest.TestCase):
     def test_sub_bytes(self):
         for i in range(0,3):
-            # buffer = random.randbytes(16)
-            # block_c = bytes(buffer)
-            # plain_state = bytes2matrix(buffer)
-            # rijndael.sub_bytes(block_c)
+            buffer = random.randbytes(16)
+            block_c = bytes(buffer)
+            plain_state = bytes2matrix(buffer)
+            rijndael.sub_bytes(block_c)
             
-            sub_bytes([])
-            # flat_list = [item for sublist in plain_state for item in sublist]
-            # block_python = bytes(flat_list)
-            # self.assertEqual(block_python, block_c)
+            sub_bytes(plain_state)
+            flat_list = [item for sublist in plain_state for item in sublist]
+            block_python = bytes(flat_list)
+            self.assertEqual(block_python, block_c)
 
 
 
