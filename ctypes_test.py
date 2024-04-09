bf1731340e954bd58bbfc3dd665829ad5a6b31ca
call203 <call203@daum.net>
2024-04-09 15:39:42 +0100

    test sub_bytes

--- ctypes_test.py
+++ ctypes_test.py
@@ -1,6 +1,4 @@
-
-# requring install pytest
-from aes.aes import sub_bytes, bytes2matrix
+from aes.aes import sub_bytes, bytes2matrix, shift_rows
 import unittest
 import random
 import ctypes
@@ -12,15 +10,15 @@ class TestEncryption(unittest.TestCase):
     def test_sub_bytes(self):
         for i in range(0,3):
             buffer = random.randbytes(16)
-            block = bytes(buffer)
+            block_c = bytes(buffer)
             plain_state = bytes2matrix(buffer)
-            rijndael.sub_bytes(block)
+            rijndael.sub_bytes(block_c)
             sub_bytes(plain_state)
-            flat_integers_list = [item for sublist in plain_state for item in sublist]
-            byte_seq = bytes(flat_integers_list)
-            self.assertEqual(byte_seq, block)
-        
-    # def test_sub_bytes():
+            flat_list = [item for sublist in plain_state for item in sublist]
+            block_python = bytes(flat_list)
+            self.assertEqual(block_python, block_c)
 
-        
 
+
+if __name__ == '__main__':
+    unittest.main()
